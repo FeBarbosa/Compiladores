55e71de3ef8822495c8d51a956faebce466c599f
Rosivania <rosivania.r1@gmail.com>
2017-05-01 19:54:45 -0300

    Trabalho 04: Iniciando GUI

---
+++ 04-Analisador-Lexico/dfa.h
@@ -0,0 +1,59 @@
+#ifndef DFA_H
+#define DFA_H
+
+#include "estado.h"
+#include "expreregular.h"
+#include "nfa.h"
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+
+class DFAEstado;
+
+//----------------------------------------------------------
+class DFAEstado : public estado
+{
+public:
+
+    using estado::estado; // Permite o uso de apenas "estado" como construtor dentro deste escopo
+    std::map<uchar, DFAEstado*> transicoes; //Tabela de Transicoes
+};
+//---------------------------------------------------------------
+
+using DFATable = std::deque<DFAEstado*>;
+
+
+struct comp
+{
+    bool operator () (NFAEstado* a, NFAEstado* b)const
+    {
+        return a->id < b->id;
+    }
+};
+
+//---------------------------------------------------------------
+class DFA
+{
+public:
+    DFA(const NFATable&, const std::vector<uchar>&);
+
+    //MÉTODOS ----------------------------------------------------
+    NFAEstado* moveFecho(NFAEstado* estadoAtual, uchar simbolo);
+    std::unordered_set<NFAEstado*> calculaFecho(NFAEstado*);
+    void CriaEstadoDeErro();
+    void criarDFA();
+    void show();
+    void renomeiaEstados();
+
+    //ATRIBUTOS ---------------------------------------------
+    NFATable nfa;
+    DFATable dfa;
+    DFAEstado *estadoErro;
+    std::map<NFAEstado*, std::unordered_set<NFAEstado*>, comp> fechos;
+    std::vector<uchar> Alfabeto;        /** Alfabeto */
+    int idAtual;
+};
+
+#endif // DFA_H
---
+++ 04-Analisador-Lexico/estado.h
@@ -0,0 +1,20 @@
+#ifndef ESTADO_H
+#define ESTADO_H
+
+//-----------------------------------------------------------------
+class estado
+{
+    public:
+
+        //CONSTRUTOR -----------------------------------------------
+        estado (int id=-1, bool estadoFinal =false, bool estadoErro = false, bool fecho=false):
+            id(id), estadoFinal(estadoFinal), estadoErro(estadoErro),fecho(fecho){}
+        //----------------------------------------------------------
+
+        int id;
+        bool estadoFinal;
+        bool estadoErro;
+        bool fecho;
+};
+//-------------------------------------------------------------------
+#endif // ESTADO_H
