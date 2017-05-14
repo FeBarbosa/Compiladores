04b08a7d8ce2685a7a8d9c1c6f92319394b5e6e1
Rosivania <rosivania.r1@gmail.com>
2017-05-14 10:58:44 -0300

    Trabalho 05: Organizando GUI

--- 05-First-e-Follow/mainwindow.cpp
+++ 05-First-e-Follow/mainwindow.cpp
@@ -12,3 +12,14 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_ok_clicked()
+{
+    std::string strV, strT, strP, strS;
+
+    strV = ui->entradaV->text().toStdString();
+    strT = ui->entradaT->text().toStdString();
+    strS = ui->entradaS->text().toStdString();
+    strP = ui->entradaP->toPlainText().toStdString();
+    char auxiliar[] = strP.c_str();
+}
